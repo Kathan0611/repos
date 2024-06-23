b02884f9f939f65bbdf6db050a8c2ee5d0a412e9
Kathan0611 <adalajakathan06@gmail.com>
2024-06-23 23:30:27 +0530

    last commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const connect = ()=>{
-     
+     console.log(process.env.DATABASE_URL)
     mongoose.connect(`${process.env.DATABASE_URL}`).then(()=>{
       console.log('Database connected successfully');
     }).catch(err=>console.log(err.message))
